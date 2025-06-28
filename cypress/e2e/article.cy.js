19471dbd2a7d97445acc12a55560619d776f26cb
Vitalii_B <vitalii.bondarenko.ua@gmail.com>
2025-06-28 22:48:44 +0300

    add tests

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,28 @@
-describe('', () => {
+const { faker } = require('@faker-js/faker');
+
+describe('Article Flow', () => {
+  let user;
+  const title = faker.lorem.words(4);
+  const description = faker.lorem.sentence();
+  const body = faker.lorem.paragraphs(2);
+
   before(() => {
+    cy.task('generateUser').then((newUser) => {
+      user = newUser;
 
+      cy.login(user.email, user.username, user.password);
+      cy.createArticle(title, description, body);
+    });
   });
 
-  it('', () => {
-
-  });
-
-  it('', () => {
-
+  it('should create and delete the article', () => {
+    cy.visit('/');
+    cy.contains(user.username.toLowerCase()).click();
+    cy.contains(title).click();
+    cy.contains('Delete Article').click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}`);
+    cy.contains(user.username.toLowerCase()).click();
+    cy.reload();
+    cy.contains(title).should('not.exist');
   });
 });
